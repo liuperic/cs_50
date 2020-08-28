e1992e5e9a6c7549e7e6c0808c829265c4e16adc
Eric Liu <liu.p.eric@gmail.com>
2020-08-28 12:16:42 -0700

    PSet 6 mario more

---
+++ pset6/mario/more/mario.py
@@ -0,0 +1,13 @@
+from cs50 import get_int
+
+height = 0
+while height < 1 or height > 8:
+    height = get_int('Height: ')
+
+for row in range(height):
+    num_space = height - row - 1
+    num_hash = row + 1
+    print(' ' * num_space, end='')
+    print('#' * num_hash, end='  ')
+
+    print('#' * num_hash)
