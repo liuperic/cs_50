30c492008487018fe4cd9b594eb0438b042426c0
Eric Liu <liu.p.eric@gmail.com>
2020-09-01 02:18:26 -0700

    PSet 7 movies

---
+++ pset7/movies/13.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT name FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE name != 'Kevin Bacon' and movies.id IN
+(SELECT movies.id FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE name = 'Kevin Bacon' AND birth = 1958)
